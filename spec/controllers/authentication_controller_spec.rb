742099e2123be51379c776dd8921d0901fda0f56
Aurian Guerard des Lauriers <aurian@hotmail.fr>
2019-11-24 20:49:35 +0100

    train change

---
+++ spec/controllers/authentication_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe AuthenticationController, type: :controller do
+  describe "POST /auth/login" do
+    let!(:user) { create(:user) }
+
+    let(:headers) {}
+  end
+  
+end
